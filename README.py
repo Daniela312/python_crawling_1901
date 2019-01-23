9c6bcfbe2731a4cb7f1a8c98bfc9a558ab0fbda9
Daniela312 <address>
2019-01-23 23:10:15 +0900

    + NewsScraping

--- README.py
+++ README.py
@@ -4,8 +4,29 @@ from bs4 import BeautifulSoup
 import urllib.request
 import re
 
-TOP10_FILE_NAME = 'Top10News.hwp'
+TOP10_FILE_NAME = 'Top10News.txt'
+NewsScraping = 'NewsScraping.txt'
 URL = "https://news.naver.com/main/list.nhn?mode=LSD&mid=sec&sid1=001"
+
+def clean_text(text):
+    cleaned_text = re.sub('[a-zA-Z]', '', text)
+    # cleaned_text = re.sub('[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]', '', cleaned_text))
+    return cleaned_text
+
+def newsScraping(url_news):
+    with open(NewsScraping, 'a', encoding='UTF-8', newline='') as f:
+
+        source_code = urllib.request.urlopen(url_news)
+        soup = BeautifulSoup(source_code, 'html.parser', from_encoding='utf-8')
+
+        text = ''
+        for item in soup.find_all('div', id='articleBodyContents'):
+            text = text + str(item.find_all(text=True))
+         
+        f.write(clean_text(text))
+        f.write("\n\n\n")
+        f.close
+
 
 #function of crawling
 def getTop10News(URL):
@@ -23,6 +44,7 @@ def getTop10News(URL):
         a = line.find('a')
         link = a.attrs['href']
         title = a.attrs['title']
+        newsScraping(link)
 
         #Write on the Text
         f.write(link)
@@ -32,7 +54,6 @@ def getTop10News(URL):
 
     f.close()
 
-    
 
 def main():
     getTop10News(URL)
